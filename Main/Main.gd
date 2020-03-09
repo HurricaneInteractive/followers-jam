9eab16da2b29f3e5182c9fd962c09e7bd5566dbb
HurricaneInteractive <hurricane.interactive@gmail.com>
2020-03-09 15:48:15 +1100

    show ending text / win state check

--- Main/Main.gd
+++ Main/Main.gd
@@ -25,18 +25,19 @@ onready var player_path = $PlayArea/PlayerPath
 onready var tilemap = $PlayArea/Tileset
 onready var player = $PlayArea/Player
 onready var score_value = $UIArea/UIWrap/ScoreValue
-
-# Evaluates if the user followed the path correctly
-func evaluate_path_taken() -> bool:
-	return true
+onready var outcome = $PlayArea/Outcome
 
 # triggers when state is changed to NEW_LEVEL
 func on_change_to_new_level() -> void:
+	outcome.visible = false
 	player_path.visible = false
 	memory_path.visible = false
 	player.visible = false
+	
 	memory_path.clear_points()
 	player_path.clear_points()
+	tilemap.clear_tileset()
+	
 	score_value.text = str(score)
 	
 	if score >= medium_threshold and score < hard_threshold:
@@ -66,6 +67,7 @@ func on_change_to_memorise() -> void:
 
 # triggers when state is changed to GAMEPLAY
 func on_change_to_gameplay() -> void:
+	memorise_timer.stop()
 	memory_path.visible = false
 	player.can_move = true
 	player.visible = true
@@ -75,18 +77,22 @@ func on_change_to_gameplay() -> void:
 
 # triggers when state is changed to FAIL
 func on_change_to_fail() -> void:
+	gameplay_timer.stop()
+	outcome.get_node("OutcomeText").text = "OOPS!"
+	outcome.visible = true
 	player.can_move = false
 	score = 0
 	Globals.change_difficulty(Globals.Difficulty.EASY)
-#	change_game_state(States.NEW_LEVEL)
 	memory_path.visible = true
 	player_path.visible = true
 
 # triggers when state is changed to WIN
 func on_change_to_win() -> void:
+	gameplay_timer.stop()
+	outcome.get_node("OutcomeText").text = "NICE!"
+	outcome.visible = true
 	player.can_move = false
 	score += 1
-	change_game_state(States.NEW_LEVEL)
 
 # method to change the state of the game
 func change_game_state(state: int) -> void:
@@ -116,12 +122,20 @@ func _process(delta):
 			timer_seconds.text = str(round(memorise_timer.time_left))
 		States.GAMEPLAY:
 			timer_seconds.text = str(round(gameplay_timer.time_left))
-#			if the player reaches the finishing point
-#				and hasn't failed, then move to WIN
+			var player_current_length = player_path.get_point_count()
+			var memory_current_length = memory_path.get_point_count()
+			var player_last_point = player_path.get_point_position(player_current_length - 1)
+			var memory_point = memory_path.get_point_position(memory_current_length - 1)
+			
+			if player_last_point[0] == memory_point[0] and player_last_point[1] == memory_point[1]:
+				change_game_state(States.WIN)
 
 func _input(event):
-	if Input.is_action_just_pressed("ui_select") && current_state == States.NEW_LEVEL:
-		change_game_state(States.MEMORISE)
+	if Input.is_action_just_pressed("ui_select"):
+		if current_state == States.NEW_LEVEL:
+			change_game_state(States.MEMORISE)
+		if current_state == States.FAIL or current_state == States.WIN:
+			change_game_state(States.NEW_LEVEL)
 
 # When the memorise timer runs out
 func _on_MemoriseTimer_timeout():
